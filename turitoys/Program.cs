4520d224463eab5124c3ae3aff4f2c1e562a0e9a
riposin <ripostf@gmail.com>
2025-03-07 22:48:02 -0600

    feat: Check how many materials are not present in db against db

--- turitoys/Program.cs
+++ turitoys/Program.cs
@@ -1,8 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Microsoft.VisualBasic.FileIO;
+﻿using Microsoft.VisualBasic.FileIO;
 using MySqlConnector;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Data;
 using System.Data.Common;
 
@@ -38,6 +36,11 @@ Console.ReadLine();
 static void optiona()
 {
 	string? option;
+	DataTable csvData = new();
+	MySqlConnection cnn = new("Server=lapqa.hamachi;User ID=root;Password=Brutus22;database=gts");
+	string? materialSKU = "";
+	string sqlGetMaterial = "select itemcode as sku, itemname as des, s4h_id as mat from oitm where oitm.ItemCode = '{0}';";
+	MySqlCommand command;
 
 	Console.Clear();
 	Console.WriteLine("""
@@ -58,19 +61,16 @@ static void optiona()
 
 	if (option != "y") { return; }
 
-
-	DataTable csvData = new();
-	//string jsonString;
-	MySqlConnection cnn = new("Server=lapqa.hamachi;User ID=root;Password=Brutus22;database=gts");
-
 	try
 	{
 		using TextFieldParser csvReader = new("C:\\Users\\JesúsRicardoPoolPech\\Documents\\QA\\z_PrerequisitosInsumos\\PreciosMasivo\\optionain.csv");
+		csvReader.TextFieldType = FieldType.Delimited;
 		csvReader.SetDelimiters(new string[] { "," });
 		csvReader.HasFieldsEnclosedInQuotes = true;
 		string[]? colFields;
 		bool tableCreated = false;
 		colFields = csvReader.ReadFields();
+
 		if (colFields != null)
 		{
 			while (tableCreated == false)
@@ -91,7 +91,7 @@ static void optiona()
 				itemFields = csvReader.ReadFields();
 				if (itemFields != null)
 				{
-					csvData.Rows.Add();
+					csvData.Rows.Add(itemFields);
 				}
 			}
 		}
@@ -111,26 +111,102 @@ static void optiona()
 	}
 	catch (Exception e)
 	{
-		Console.WriteLine("Error during connection: " + e.Message);
+		Console.WriteLine("MySQL: Error during connection - " + e.Message);
 		return;
 	}
 
-	var command = new MySqlCommand("select itemcode as sku, itemname as des, s4h_id as mat from oitm where oitm.ItemCode = '4005800144592';", cnn);
+	int notFoundItems = 0;
 
-	var reader = command.ExecuteReader();
-	List<string[]> result = [];
+	// SKU always must be first element, check linq.first statement when verifying
+	command = new MySqlCommand("select itemcode as sku, itemname as des, s4h_id as mat from oitm;", cnn);
 
-	while (reader.Read())
+	var readerAll = command.ExecuteReader();
+	List<string[]> resultAll = [];
+	IReadOnlyCollection<DbColumn> columns = readerAll.GetColumnSchema();
+	Console.WriteLine("Getting all materials from db to process locally, please wait...");
+
+	while (readerAll.Read())
 	{
 		string[] item = [];
-		foreach (DbColumn col in reader.GetColumnSchema())
+		foreach (DbColumn col in columns)
 		{
-			item = [.. item, (string)reader[col.ColumnName]];
+			item = [.. item, (string)(readerAll[col.ColumnName] == DBNull.Value ? "" : readerAll[col.ColumnName])];
 		}
-
-		result.Add(item);
+		resultAll.Add(item);
 	}
-	Console.WriteLine(result.Count == 0 ? "Not found" : result[0][2]);
+	readerAll.Close();
+
+	if (resultAll.Count == 0)
+	{
+		Console.WriteLine("No materials found");
+	}
+	else
+	{
+		Console.WriteLine("Found " + resultAll.Count + " materials");
+	}
+
+	Console.WriteLine("Verifying if materials in CSV exists in materials retrieved from db, please wait...");
+	foreach (DataRow mat in csvData.Rows)
+	{
+		materialSKU = String.IsNullOrEmpty(mat["sku"].ToString()) ? "" : mat["sku"].ToString();
+		var query = resultAll.Where(arr => arr.First() == materialSKU);
+		if(query.Count() == 0)
+		{
+			notFoundItems++;
+			// echo the items not found
+		}
+		else
+		{
+			// get the materials from query and compare sku
+			// if didn't match, set the s4hid
+		}
+	}
+	Console.WriteLine(notFoundItems > 0 ? notFoundItems + " materials were not found in db of " + csvData.Rows.Count + " in csv" : "All materials were found");
+
+
+
+	// One quey per material directly to db, not efficient!
+	foreach (DataRow mat in csvData.Rows)
+	{
+		/*
+		aux ++;
+		materialSKU = String.IsNullOrEmpty(mat["sku"].ToString()) ? "" : mat["sku"].ToString();
+		command = new MySqlCommand(string.Format(sqlGetMaterial, materialSKU), cnn);
+
+		var reader = command.ExecuteReader();
+		List<string[]> result = [];
+
+		while (reader.Read())
+		{
+			string[] item = [];
+			foreach (DbColumn col in reader.GetColumnSchema())
+			{
+				item = [.. item, (string)reader[col.ColumnName]];
+			}
+			result.Add(item);
+		}
+		reader.Close();
+
+		if (result.Count == 0)
+		{
+			notFoundItems ++;
+			Console.WriteLine("Not found " + materialSKU);
+		}
+		else
+		{
+			Console.WriteLine("Item code for " + materialSKU + ": " + result[0][2]);
+		}
+		*/
+
+		//Console.WriteLine(result.Count == 0 ? "Not found " + materialSKU : "Item code for " + materialSKU + ": " + result[0][2]);
+
+		/*if (aux > 10)
+		{
+			break;
+		}*/
+	}
+
+	Console.WriteLine("Not found items count: " + notFoundItems);
 
 	cnn.Close();
 
